8a72abb4d614b803a9eba605616357224e5bba1e
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2020-04-26 21:43:30 +0300

    update tests

--- conversable/test_converse.py
+++ conversable/test_converse.py
@@ -8,9 +8,21 @@ data_dir = os.path.join(conversable_dir, 'data')
 utils.logging_setup()
 
 sample_text = [
+    # [
+    #     "Captain, I'm glad you got my message. I been thinking on this mission. They took the prince, they criminals. They kidnap and they trying to rob our ships. Maybe they also liars. Maybe they trick us. Maybe they make up the story. Maybe no kingdom in east, maybe never was.",
+    #     "Cap'n, I'ma glad you god ma message. I been dinkin on dis mission. Dey dook da prince, dey criminals. Dey kidnap and dey drying do rob our ships. Maybe dey also liars. Maybe dey drick us. Maybe dey make up da sdory. Maybe no kingdom in easd, maybe never was."
+    # ],
     [
-        "Captain, I'm glad you got my message. I been thinking on this mission. They took the prince, they criminals. They kidnap and they trying to rob our ships. Maybe they also liars. Maybe they trick us. Maybe they make up the story. Maybe no kingdom in east, maybe never was.",
-        "Cap'n, I'ma glad you god ma message. I been dinkin on dis mission. Dey dook da prince, dey criminals. Dey kidnap and dey drying do rob our ships. Maybe dey also liars. Maybe dey drick us. Maybe dey make up da sdory. Maybe no kingdom in easd, maybe never was."
+        "how do you do captain?",
+        "'ow diya do cap'n?"
+    ],
+    [
+        "this is a good example wouldn't you say?",
+        "dis is a good example wouldn'd you say?"
+    ],
+    [
+        "Here, this is my favorite.",
+        "'ere, dis is ma favoride."
     ],
     # [
     #     "Here is the report captain. Everything is written down for you, down to the last screw. I have made a notation for each tool and supplies what they are for, and you can also find the tools and supplies referenced under each device or ship part. All in all sir, we are in good stock, but since we expect a long journy and i suspect unexpected surprises. Then I recomend we double the standard supply ammounts for everything to make sure we have enough on board, and that we should tripple the type-C screws, those are used for hull repairs, and that would be the last thing we want to run out of sir.",
